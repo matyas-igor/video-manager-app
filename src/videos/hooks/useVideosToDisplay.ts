1c29db8683f064f691ad6ce7b43512f8c55e6bad
Igor Matias <matyas.igor@gmail.com>
2021-01-18 14:57:52 +0300

    feat: implement actual sorting

--- src/videos/hooks/useVideosToDisplay.ts
+++ src/videos/hooks/useVideosToDisplay.ts
@@ -2,12 +2,20 @@ import fuzzysort from 'fuzzysort';
 import { Order, OrderBy, VideoProcessed } from '../../common/interfaces';
 import { useMemo } from 'react';
 
+const sorters: { [orderBy in OrderBy]: (videos: VideoProcessed[], order: Order) => VideoProcessed[] } = {
+  name: (videos: VideoProcessed[], order: Order) =>
+    videos.sort((a, b) => (order === 'desc' ? -1 : 1) * (a.name.localeCompare(b.name) || a.author.localeCompare(b.author))),
+  author: (videos: VideoProcessed[], order: Order) =>
+    videos.sort((a, b) => (order === 'desc' ? -1 : 1) * (a.author.localeCompare(b.author) || a.name.localeCompare(b.name))),
+};
+
 export const useVideosToDisplay = (videos: VideoProcessed[], search: string, orderBy: OrderBy, order: Order): VideoProcessed[] => {
   return useMemo(() => {
-    return !search
+    const videosSearched = !search
       ? videos
       : fuzzysort
           .go<VideoProcessed>(search, videos, { key: 'search' })
           .map(({ obj }) => obj);
-  }, [videos, search]);
+    return sorters[orderBy](videosSearched, order);
+  }, [videos, search, orderBy, order]);
 };
